960654fde79211680820d40b4f6d7507f1b84337
TakaHacker <matheus8antunes@gmail.com>
2019-10-12 19:33:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void MudaTela(View v){
+    Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
